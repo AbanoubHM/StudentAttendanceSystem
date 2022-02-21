6263c2466d72c42691438c0c1ce97591428f0a05
hussain mabeed <hussainmabeed@gmail.com>
2022-02-21 23:04:38 +0200

    edit student data

--- StudentAttendanceSystem/StudentData.Designer.cs
+++ StudentAttendanceSystem/StudentData.Designer.cs
@@ -29,10 +29,17 @@ namespace StudentAttendanceSystem
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // StudentData
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(873, 536);
+            this.Name = "StudentData";
             this.Text = "StudentData";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
