4e5d09fc5242058e6ac9bec5c49e6d5b9042bfcc
hussain mabeed <hussainmabeed@gmail.com>
2022-02-23 00:18:29 +0200

    small edit in reports

--- StudentAttendanceSystem/Report_Student_Data_save.Designer.cs
+++ StudentAttendanceSystem/Report_Student_Data_save.Designer.cs
@@ -30,38 +30,26 @@ namespace StudentAttendanceSystem
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.dataGridView2 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 15);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(895, 460);
+            this.dataGridView1.Size = new System.Drawing.Size(920, 563);
             this.dataGridView1.TabIndex = 0;
-            // 
-            // dataGridView2
-            // 
-            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(803, 454);
-            this.dataGridView2.Name = "dataGridView2";
-            this.dataGridView2.Size = new System.Drawing.Size(240, 150);
-            this.dataGridView2.TabIndex = 1;
             // 
             // Report_Student_Data_save
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(919, 544);
-            this.Controls.Add(this.dataGridView2);
+            this.ClientSize = new System.Drawing.Size(968, 640);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Report_Student_Data_save";
             this.Text = "Report_Student_Data_save";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -69,6 +57,5 @@ namespace StudentAttendanceSystem
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridView dataGridView2;
     }
 }
