76a42366188a00dd4c003b72daad2a45ce825a87
Fady Shafeek <Fady.Shafeek@hotmail.com>
2022-02-23 04:55:06 +0200

    Update DailyAttend.cs
    Adding Date instead of DateTime()

--- StudentAttendanceSystem/Models/DailyAttend.cs
+++ StudentAttendanceSystem/Models/DailyAttend.cs
@@ -1,6 +1,8 @@
 ﻿using StudentAttendanceSystem.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +13,7 @@ namespace StudentAttendanceSystem
     public class DailyAttend
     {
         public int DailyAttendID { get; set; }
+        [Column(TypeName = "date")]
         public DateTime ADate { get; set; }
         public ICollection<Attend> Attends { get; set; }
     }
