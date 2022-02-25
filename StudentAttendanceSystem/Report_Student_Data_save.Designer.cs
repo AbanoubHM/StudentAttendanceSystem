31d0f3e718a6702b0f83a0e7179db08dc9701c34
Fady Shafeek <Fady.Shafeek@hotmail.com>
2022-02-25 04:36:54 +0200

    Base

--- StudentAttendanceSystem/Report_Student_Data_save.Designer.cs
+++ StudentAttendanceSystem/Report_Student_Data_save.Designer.cs
@@ -45,13 +45,12 @@ namespace StudentAttendanceSystem
             // 
             // crystalReportViewer1
             // 
-            this.crystalReportViewer1.ActiveViewIndex = 0;
+            this.crystalReportViewer1.ActiveViewIndex = -1;
             this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
             this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.crystalReportViewer1.Location = new System.Drawing.Point(0, 0);
             this.crystalReportViewer1.Name = "crystalReportViewer1";
-            this.crystalReportViewer1.ReportSource = this.StudentsData1;
             this.crystalReportViewer1.Size = new System.Drawing.Size(994, 569);
             this.crystalReportViewer1.TabIndex = 1;
             this.crystalReportViewer1.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None;
